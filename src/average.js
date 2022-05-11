4b1934c1228fee9894415630bd51cc7161f8d71b
David Junior <david.lourenco94@gmail.com>
2022-05-11 16:30:00 -0300

    Função average OK

--- src/average.js
+++ src/average.js
@@ -14,10 +14,10 @@
 const average = (array) => {
   let soma = 0;
   let media = 0;
-  if (array.length == 0) {
+  if (array.length === 0) {
     return undefined;
   }
-  for(let i = 0; i < array.length; i += 1){
+  for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
       return undefined;
     }
