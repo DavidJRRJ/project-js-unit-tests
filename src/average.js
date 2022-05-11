c860569f0a09ee2e83deace74f62868ad5813530
David Junior <david.lourenco94@gmail.com>
2022-05-11 16:27:19 -0300

    Função average OK

--- src/average.js
+++ src/average.js
@@ -8,9 +8,23 @@
   Comportamento:
     - average([2, 2]) // Retorno: 2;
     - average([1, 1]) // Retorno: 1;
-    - average([1, '2']) // Retorno: undefined; primeiro commit
+    - average([1, '2']) // Retorno: undefined; 
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  let media = 0;
+  if (array.length == 0) {
+    return undefined;
+  }
+  for(let i = 0; i < array.length; i += 1){
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    soma += array[i];
+  }
+  media = Math.round(soma / array.length);
+  return media;
+};
 
 module.exports = average;
