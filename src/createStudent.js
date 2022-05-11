e991b4d163899f935aec43949c1fa115cf89055a
David Junior <david.lourenco94@gmail.com>
2022-05-11 18:35:25 -0300

    Função createStudent OK

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (texto) => {
+  let objeto = {};
+  objeto.name = texto;
+  objeto.feedback = () => 'Eita pessoa boa!';
+  return objeto;
+};
 
 module.exports = createStudent;
